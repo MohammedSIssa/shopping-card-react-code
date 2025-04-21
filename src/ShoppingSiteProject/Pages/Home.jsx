25d151dee0ad222fd0d3f2f7c4d462fd8d19c413
MohammedSIssa <47881107+MohammedSIssa@users.noreply.github.com>
2025-04-21 09:31:20 +0300

    Update Home.jsx

--- src/ShoppingSiteProject/Pages/Home.jsx
+++ src/ShoppingSiteProject/Pages/Home.jsx
@@ -1,16 +1,16 @@
 import '../components/styles/home.css'
 import { NavLink } from 'react-router-dom';
-import { DynamicIcon } from 'lucide-react/dynamic';
+import { MoveUpRight } from 'lucide-react';
 
 function Home() {
   return (
     <div className="home-page">
       <h1>Buy Whatever makes you Happy!</h1>
-      <button><NavLink to='/shop'>Visit our shop <DynamicIcon name='move-up-right' /> </NavLink></button>
+      <button><NavLink to='/shop'>Visit our shop <MoveUpRight /></NavLink></button>
     </div>
   )
 }
 
-// TODO: [] Make a good-looking home page.
+// TODO: [x] Make a good-looking home page.
 
 export default Home;
